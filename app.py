b993bdd38b3eb1fc4ca29cf3c8ac79b6c534b02d
pau <97336249+abcpau@users.noreply.github.com>
2025-03-06 01:49:47 +0800

    feat: display input

--- app.py
+++ app.py
@@ -25,6 +25,9 @@ if "pred" not in st.session_state:
 
 st.title("FAKE NEWS DETECTOR!")
 
+claim_placeholder = st.empty()
+premise_placeholder = st.empty()
+
 col1,col2 = st.columns([1,2])
 
 
@@ -38,13 +41,13 @@ def display_pred():
 with st.form("my_form"):
 
     claim = st.text_area(
-        "Claim",
+        "Input Claim",
         key="claim",
         placeholder="Enter the statement you want to verify here...",
     )
 
     premise = st.text_area(
-        "Premise",
+        "Input Premise",
         key="premise",
         placeholder="Enter some facts to help us verify your claim here...",
     )
@@ -52,5 +55,7 @@ with st.form("my_form"):
     submit = st.form_submit_button('Verify', on_click=display_pred)
 
 if submit:
-    col1.header('Verdict:')
-    col2.header(f"*{st.session_state.pred.capitalize()}*")
+    claim_placeholder.markdown(f"Claim: {st.session_state.claim}")
+    premise_placeholder.markdown(f"Premise: {st.session_state.premise}")
+    col1.markdown("<h2 style='padding-top: 0px;'>Verdict: </h2>", unsafe_allow_html=True)
+    col2.markdown(f"<h2 style='padding-top: 0px; color:red'> <em>{st.session_state.pred.capitalize()}</em> </h2>", unsafe_allow_html=True)

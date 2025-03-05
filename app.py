a61192e071ec23c4fa07c7148cc6c2c980964400
pau <97336249+abcpau@users.noreply.github.com>
2025-03-06 00:59:01 +0800

    feat: add initial ui

--- app.py
+++ app.py
@@ -1,3 +1,56 @@
 import streamlit as st
 
-st.write("Hello world")
+
+#------------------
+def predict(h, p):
+    prediction = 'fact'
+    # prediction = 'false'
+    # st.write(h, p)
+    return prediction
+#------------------
+
+
+if "claim" not in st.session_state:
+    st.session_state.claim = ""
+
+if "premise" not in st.session_state:
+    st.session_state.premise = ""
+    
+if "pred" not in st.session_state:
+    st.session_state.pred = ""
+
+
+#------------------
+
+
+st.title("FAKE NEWS DETECTOR!")
+
+col1,col2 = st.columns([1,2])
+
+
+#------------------
+
+def display_pred():
+    st.session_state.pred = predict(st.session_state.claim, st.session_state.premise)
+
+#------------------
+
+with st.form("my_form"):
+
+    claim = st.text_area(
+        "Claim",
+        key="claim",
+        placeholder="Enter the statement you want to verify here...",
+    )
+
+    premise = st.text_area(
+        "Premise",
+        key="premise",
+        placeholder="Enter some facts to help us verify your claim here...",
+    )
+
+    submit = st.form_submit_button('Verify', on_click=display_pred)
+
+if submit:
+    col1.header('Verdict:')
+    col2.header(f"*{st.session_state.pred.capitalize()}*")

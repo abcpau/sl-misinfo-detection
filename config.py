5070028ee8e78ced54d894fa970e209ba77eefdb
pau <97336249+abcpau@users.noreply.github.com>
2025-03-19 21:48:34 +0800

    chore: add config file

---
+++ config.py
@@ -0,0 +1,7 @@
+IP_ADDRESS = '10.147.36.147'
+PORT = 11434
+URL = f"http://{IP_ADDRESS}:{PORT}/api/chat"
+HEADERS = {"Content-Type": "application/json"}
+
+MODEL = "llama3.1:8b"
+# MODEL = "deepseek-r1:8b" # Not yet supported

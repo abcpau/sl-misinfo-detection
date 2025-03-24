c2ba96881e8ac9dce1cb75c85e7dd2385107208d
Ma. Pauline Abcede <97336249+abcpau@users.noreply.github.com>
2025-03-24 16:39:42 +0800

    feat: display premise finder details

--- app.py
+++ app.py
@@ -3,8 +3,39 @@ import config
 from backend import format_premise_hypothesis, generate_inference, parse_llama_explanation
 
 #------------------
+def find_premise_via_sentence_similarity():
+    top_results = [("Premise 1", 0.7), ("Premise 2", 0.5),]
+    prem = top_results[0][0]
+    prem_similarity = top_results[0][1]
+    return top_results, prem, prem_similarity
+
+def find_premise_via_webrag():
+    websearch = [{"title": "Title 1",
+                 "href": "https://verafiles.org/",
+                 "body": "Body 1",
+                 "logo_url": "https://upload.wikimedia.org/wikipedia/commons/b/b8/VeraFiles_logo.svg"},
+                 {"title": "Title 2",
+                 "href": "https://verafiles.org/",
+                 "body": "Body 2",
+                 "logo_url": "https://upload.wikimedia.org/wikipedia/commons/b/b8/VeraFiles_logo.svg"}]
+    return websearch, "https://verafiles.org/"
+
+def find_premise_via_webcrawl():
+    websearch = [{"title": "Title 1",
+                 "href": "https://verafiles.org/",
+                 "body": "Body 1",
+                 "logo_url": "https://upload.wikimedia.org/wikipedia/commons/b/b8/VeraFiles_logo.svg"},
+                 {"title": "Title 2",
+                 "href": "https://verafiles.org/",
+                 "body": "Body 2",
+                 "logo_url": "https://upload.wikimedia.org/wikipedia/commons/b/b8/VeraFiles_logo.svg"}]
+    return websearch, "<>", "This is the PREMISE"
+
 
 def predict(h, p):
+    #--------------
+    return "False", "This is the EXPLANATION"
+    #--------------
     formatted_input = format_premise_hypothesis(p, h)
     inference = generate_inference(url=URL, headers=HEADERS, model=MODEL, content=formatted_input)
     prediction, explanation = parse_llama_explanation(inference)
@@ -16,7 +47,7 @@ if "claim" not in st.session_state:
     st.session_state.claim = ""
 
 if "premise_finder" not in st.session_state:
-    st.session_state.premise_finder = ""
+    st.session_state.premise_finder = None
 
 if "premise" not in st.session_state:
     st.session_state.premise = ""
@@ -34,6 +65,9 @@ if "expl" not in st.session_state:
 st.title("FAKE NEWS DETECTOR!")
 
 claim_placeholder = st.empty()
+top_prems_placeholder = st.empty()
+websearch_placeholder = st.empty()
+markdown_placeholder = st.empty()           # to remove
 premise_placeholder = st.empty()
 
 col1,col2 = st.columns([1,2])
@@ -43,57 +77,61 @@ expl_placeholder = st.empty()
 
 #------------------
 
-def shorten_premise_finder():
-    pf = ""
-    match st.session_state.premise_finder:
-        case "I have my own premise":
-            pf = "own"
-        case "Check your dataset":
-            pf = "pomi"
-        case "Refer to the Internet using WebRAG":
-            pf = "webrag"
-        case _:
-            pf = "webcrawl"
-    return pf
-
 def display_pred():
-    pf = shorten_premise_finder()
-    if pf == "own":
+    st.session_state.premise_finder_index = premise_finder_options.index(premise_finder)
+    pf = st.session_state.premise_finder_index
+    st.write(pf)
+
+    if pf == 0:
         pred, expl = predict(st.session_state.claim, st.session_state.premise)
         st.session_state.pred = pred
         st.session_state.expl = expl
 
-    elif pf == "pomi":
-        # prem = find_premise_via_sentence_similarity
-        prem = st.session_state.premise     #placeholder, to remove
+
+    elif pf == 1:
+        top_prems, prem = find_premise_via_sentence_similarity()    #placeholder, to remove
+        st.session_state.top_prems = top_prems 
+        st.session_state.premise = prem
+
         pred, expl = predict(st.session_state.claim, prem)
         st.session_state.pred = pred
         st.session_state.expl = expl
     
-    elif pf == "webrag":
-        # prem = find_premise_via_webrag
-        prem = st.session_state.premise     #placeholder, to remove
-        pred, expl = predict(st.session_state.claim, prem)
+
+    elif pf == 2:
+        # websearch, prem_link = find_premise_via_webrag(st.session_state.claim, url=URL, headers=HEADERS, model=MODEL)
+        websearch, prem_link = find_premise_via_webrag()            #placeholder, to remove
+        st.session_state.websearch = websearch
+        st.session_state.premise = prem_link
+
+        pred, expl = predict(st.session_state.claim, prem_link)
         st.session_state.pred = pred
         st.session_state.expl = expl
 
+
     else:
-        # prem = find_premise_via_webcrawl
-        prem = st.session_state.premise     #placeholder, to remove
+        # websearch, markdown, prem = find_premise_via_webcrawl(st.session_state.claim, url=URL, headers=HEADERS, model=MODEL)
+        websearch, markdown, prem = find_premise_via_webcrawl()     #placeholder, to remove
+        st.session_state.websearch = websearch     
+        st.session_state.markdown = markdown     
+        st.session_state.premise = prem     
+
         pred, expl = predict(st.session_state.claim, prem)
         st.session_state.pred = pred
         st.session_state.expl = expl
 
 #------------------
 
+premise_finder_options = ["I have my own premise",
+                        "Check your dataset",
+                        "Refer to the Internet using WebRAG",
+                        "Refer to the Internet using WebCrawl"]
+
 premise_finder = st.radio(
         "Where will we get the premise?",
-        ["I have my own premise",
-         "Check your dataset",
-         "Refer to the Internet using WebRAG",
-         "Refer to the Internet using WebCrawl"],
-         key="premise_finder"
-    )
+        premise_finder_options,
+         key="premise_finder",
+)
 
 
 with st.form("my_form"):
@@ -119,7 +157,43 @@ with st.form("my_form"):
 
 if submit:
     claim_placeholder.markdown(f"Claim: {st.session_state.claim}")
-    premise_placeholder.markdown(f"Premise: {st.session_state.premise}")
+
+
+    if st.session_state.premise_finder_index == 0:
+        premise_placeholder.markdown(f"Premise: {st.session_state.premise}")
+
+
+    elif st.session_state.premise_finder_index == 1:
+        with top_prems_placeholder.container():
+            st.markdown(f"Top premises:\n")
+            for prem in st.session_state.top_prems:
+                st.markdown(f"{prem[0]}: {prem[1]*100}% similar\n")
+        premise_placeholder.markdown(f"Premise: {st.session_state.premise}")
+
+
+    elif st.session_state.premise_finder_index == 2:
+        with websearch_placeholder.container():
+            st.markdown(f"Websearch results:\n", unsafe_allow_html=True)
+            for result in st.session_state.websearch:
+                st.markdown(f"""<img src="{result['logo_url']}" width='30px'>""", unsafe_allow_html=True)
+                st.markdown(f"{result['title']}\n")
+                st.markdown(f"{result['href']}\n")
+                st.markdown(f"{result['body']}\n")
+        premise_placeholder.markdown(f"Premise taken from: {st.session_state.premise}")
+
+
+    elif st.session_state.premise_finder_index == 3:
+        with websearch_placeholder.container():
+            st.markdown(f"Websearch results:\n", unsafe_allow_html=True)
+            for result in st.session_state.websearch:
+                st.markdown(f"""<img src="{result['logo_url']}" width='30px'>""", unsafe_allow_html=True)
+                st.markdown(f"{result['title']}\n")
+                st.markdown(f"{result['href']}\n")
+                st.markdown(f"{result['body']}\n")
+        markdown_placeholder.markdown(f"Markdown (temp, to remove):<br>{st.session_state.markdown}", unsafe_allow_html=True)
+        premise_placeholder.markdown(f"Premise: {st.session_state.premise}")
+
+
     col1.markdown("<h2 style='padding-top: 0px;'>Verdict: </h2>", unsafe_allow_html=True)
     if st.session_state.pred == "fact":
         col2.markdown(f"<h2 style='padding-top: 0px; color:green'> <em>{st.session_state.pred.capitalize()}</em> </h2>", unsafe_allow_html=True)
